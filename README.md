0ae0ef4ca0cd9caa48cb5d40ac84661ca227f25b
DarkCthulhu <anirudh4444@gmail.com>
2015-05-10 19:45:03 -0500

    Initial commit with requirements.txt Next: adding wiki page for setup instructions

---
+++ README.md
@@ -0,0 +1,7 @@
+##SomethingBot
+
+This is an AI that we are building in our spare time.
+It will be a console application at first and maybe later  on add more functionality.
+Our language of choice for this is Python.
+
+More details to come
---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import pyttsx
+
+"""
+This is the entry point of my AI engine.
+It is meant to call the various functions
+under various modules. 
+
+voice synthesis - pyttsx
+"""
+
+def main():
+    pass
+    
+if __name__ == '__main__':
+    main()
